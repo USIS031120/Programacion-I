956d4d7e2dc6e47190587b685ca8c90c6075d364
herson <usis031120@ugb.edu.sv>
2020-09-09 12:19:43 -0600

    Resolucion de la tarea de conversores

--- holaMundo/Form1.Designer.vb
+++ holaMundo/Form1.Designer.vb
@@ -22,55 +22,124 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lbldepto = New System.Windows.Forms.Label()
-        Me.lblmun = New System.Windows.Forms.Label()
-        Me.cboDepto = New System.Windows.Forms.ComboBox()
-        Me.cboMun = New System.Windows.Forms.ComboBox()
+        Me.lbltipo = New System.Windows.Forms.Label()
+        Me.lblde = New System.Windows.Forms.Label()
+        Me.cbotipo = New System.Windows.Forms.ComboBox()
+        Me.cbode = New System.Windows.Forms.ComboBox()
+        Me.cboa = New System.Windows.Forms.ComboBox()
+        Me.lbla = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnconvertir = New System.Windows.Forms.Button()
+        Me.lblresultado = New System.Windows.Forms.Label()
+        Me.txtcantidad = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
-        'lbldepto
+        'lbltipo
         '
-        Me.lbldepto.AutoSize = True
-        Me.lbldepto.Location = New System.Drawing.Point(20, 40)
-        Me.lbldepto.Name = "lbldepto"
-        Me.lbldepto.Size = New System.Drawing.Size(39, 13)
-        Me.lbldepto.TabIndex = 0
-        Me.lbldepto.Text = "Depto:"
+        Me.lbltipo.AutoSize = True
+        Me.lbltipo.Location = New System.Drawing.Point(234, 48)
+        Me.lbltipo.Name = "lbltipo"
+        Me.lbltipo.Size = New System.Drawing.Size(31, 13)
+        Me.lbltipo.TabIndex = 0
+        Me.lbltipo.Text = "Tipo:"
         '
-        'lblmun
+        'lblde
         '
-        Me.lblmun.AutoSize = True
-        Me.lblmun.Location = New System.Drawing.Point(222, 40)
-        Me.lblmun.Name = "lblmun"
-        Me.lblmun.Size = New System.Drawing.Size(31, 13)
-        Me.lblmun.TabIndex = 3
-        Me.lblmun.Text = "Mun:"
+        Me.lblde.AutoSize = True
+        Me.lblde.Location = New System.Drawing.Point(39, 102)
+        Me.lblde.Name = "lblde"
+        Me.lblde.Size = New System.Drawing.Size(25, 13)
+        Me.lblde.TabIndex = 3
+        Me.lblde.Text = "DE:"
         '
-        'cboDepto
+        'cbotipo
         '
-        Me.cboDepto.FormattingEnabled = True
-        Me.cboDepto.Location = New System.Drawing.Point(76, 37)
-        Me.cboDepto.Name = "cboDepto"
-        Me.cboDepto.Size = New System.Drawing.Size(121, 21)
-        Me.cboDepto.TabIndex = 4
+        Me.cbotipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbotipo.FormattingEnabled = True
+        Me.cbotipo.Items.AddRange(New Object() {"Moneda", "Longitud", "Masa", "Almacenamiento", "Tiempo"})
+        Me.cbotipo.Location = New System.Drawing.Point(271, 45)
+        Me.cbotipo.Name = "cbotipo"
+        Me.cbotipo.Size = New System.Drawing.Size(121, 21)
+        Me.cbotipo.TabIndex = 4
         '
-        'cboMun
+        'cbode
         '
-        Me.cboMun.FormattingEnabled = True
-        Me.cboMun.Location = New System.Drawing.Point(259, 37)
-        Me.cboMun.Name = "cboMun"
-        Me.cboMun.Size = New System.Drawing.Size(121, 21)
-        Me.cboMun.TabIndex = 5
+        Me.cbode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbode.FormattingEnabled = True
+        Me.cbode.Location = New System.Drawing.Point(76, 99)
+        Me.cbode.Name = "cbode"
+        Me.cbode.Size = New System.Drawing.Size(121, 21)
+        Me.cbode.TabIndex = 5
+        '
+        'cboa
+        '
+        Me.cboa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboa.FormattingEnabled = True
+        Me.cboa.Location = New System.Drawing.Point(237, 102)
+        Me.cboa.Name = "cboa"
+        Me.cboa.Size = New System.Drawing.Size(121, 21)
+        Me.cboa.TabIndex = 7
+        '
+        'lbla
+        '
+        Me.lbla.AutoSize = True
+        Me.lbla.Location = New System.Drawing.Point(214, 105)
+        Me.lbla.Name = "lbla"
+        Me.lbla.Size = New System.Drawing.Size(17, 13)
+        Me.lbla.TabIndex = 6
+        Me.lbla.Text = "A:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(378, 105)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Cantidad:"
+        '
+        'btnconvertir
+        '
+        Me.btnconvertir.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnconvertir.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnconvertir.Location = New System.Drawing.Point(76, 148)
+        Me.btnconvertir.Name = "btnconvertir"
+        Me.btnconvertir.Size = New System.Drawing.Size(143, 44)
+        Me.btnconvertir.TabIndex = 10
+        Me.btnconvertir.Text = "Convertir"
+        Me.btnconvertir.UseVisualStyleBackColor = False
+        '
+        'lblresultado
+        '
+        Me.lblresultado.AutoSize = True
+        Me.lblresultado.Location = New System.Drawing.Point(378, 167)
+        Me.lblresultado.Name = "lblresultado"
+        Me.lblresultado.Size = New System.Drawing.Size(67, 13)
+        Me.lblresultado.TabIndex = 11
+        Me.lblresultado.Text = "Resultado: ?"
+        '
+        'txtcantidad
+        '
+        Me.txtcantidad.Location = New System.Drawing.Point(437, 102)
+        Me.txtcantidad.Name = "txtcantidad"
+        Me.txtcantidad.Size = New System.Drawing.Size(125, 20)
+        Me.txtcantidad.TabIndex = 12
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(454, 201)
-        Me.Controls.Add(Me.cboMun)
-        Me.Controls.Add(Me.cboDepto)
-        Me.Controls.Add(Me.lblmun)
-        Me.Controls.Add(Me.lbldepto)
+        Me.ClientSize = New System.Drawing.Size(585, 230)
+        Me.Controls.Add(Me.txtcantidad)
+        Me.Controls.Add(Me.lblresultado)
+        Me.Controls.Add(Me.btnconvertir)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboa)
+        Me.Controls.Add(Me.lbla)
+        Me.Controls.Add(Me.cbode)
+        Me.Controls.Add(Me.cbotipo)
+        Me.Controls.Add(Me.lblde)
+        Me.Controls.Add(Me.lbltipo)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
@@ -81,8 +150,14 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents lbldepto As Label
-    Friend WithEvents lblmun As Label
-    Friend WithEvents cboDepto As ComboBox
-    Friend WithEvents cboMun As ComboBox
+    Friend WithEvents lbltipo As Label
+    Friend WithEvents lblde As Label
+    Friend WithEvents cbotipo As ComboBox
+    Friend WithEvents cbode As ComboBox
+    Friend WithEvents cboa As ComboBox
+    Friend WithEvents lbla As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnconvertir As Button
+    Friend WithEvents lblresultado As Label
+    Friend WithEvents txtcantidad As TextBox
 End Class
--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,24 +1,37 @@
 ﻿Public Class Form1
-    Dim objdirecciones = New direcciones
+    Dim objconversores = New conversores
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cboDepto.Items.AddRange(objdirecciones.depto)
+    Private Sub cboDepto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbotipo.SelectedIndexChanged
+        cbode.Items.Clear()
+        cbode.Text = ""
+        cbode.Items.AddRange(objconversores.etiquetas(cbotipo.SelectedIndex))
+
+        cboa.Items.Clear()
+        cboa.Text = ""
+        cboa.Items.AddRange(objconversores.etiquetas(cbotipo.SelectedIndex))
     End Sub
 
-    Private Sub cboDepto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboDepto.SelectedIndexChanged
-        cboMun.Items.Clear()
-        cboMun.Text = ""
-        cboMun.Items.AddRange(objdirecciones.mun(cboDepto.SelectedIndex))
+    Private Sub btnconvertir_Click(sender As Object, e As EventArgs) Handles btnconvertir.Click
+        lblresultado.Text = "Resultado: " + objconversores.convertir(cbotipo.SelectedIndex, cbode.SelectedIndex, cboa.SelectedIndex, txtcantidad.Text).ToString() + " " + objconversores.etiquetas(cbotipo.SelectedIndex)(cboa.SelectedIndex).ToString()
     End Sub
 End Class
 
-Class direcciones
-    Public depto() As String = {"Seleccione un depto.", "Usulutan", "San Miguel", "La Union", "Morazan"}
-    Public mun()() As String = {
-        New String() {"Seleccione un depto."},
-        New String() {"Seleccione un municipio", "Usulutan", "Jiquilisco", "Santa Elena", "Santa Maria"},
-        New String() {"Seleccione un municipio", "San Miguel", "El Transito", "San Rafael Ote", "San Jorge"},
-        New String() {"Seleccione un municipio", "La Union", "SLR", "Anamoros", "Bolivar"},
-        New String() {"Seleccione un municipio", "Arambula", "Perquin", "Corinto", "Cacaopera"}
-        }
+Class conversores
+    Public etiquetas()() As String = {
+        New String() {"Dolar", "Euro", "Quetzales", "Lempira", "Colon SV", "Cordoba", "Colon CR"},
+        New String() {"Metro", "CM", "Pulgadas", "Pies", "Varas", "Yardas", "Km", "Millas"},
+        New String() {"Libra", "Gramos", "Kilogramos", "Onzas", "Quintales", "Toneladas"},
+        New String() {"MB", "Bit", "Byte", "KB", "GB", "TB"},
+        New String() {"Hora", "Segundos", "Minutos", "Dias", "Semana", "Meses", "Años"}
+    }
+    Public valores()() As Double = {
+        New Double() {1, 0.85, 7.74, 24.8, 8.75, 34.6, 597.23},
+        New Double() {1, 100, 39.3701, 3.28084, 1.1963081929167, 1.09361, 0.001, 0.000621371},
+        New Double() {1, 453.592, 0.453592, 16, 0.01, 0.000453592},
+        New Double() {1, 8000000.0, 1000000.0, 1000, 0.001, 0.000001},
+        New Double() {1, 3600, 60, 0.0416667, 0.00595238, 0.00136986, 0.000114155}
+    }
+    Public Function convertir(ByVal tipo As Int16, ByVal de As Int16, ByVal a As Int16, ByVal cantidad As Double)
+        Return valores(tipo)(a) / valores(tipo)(de) * cantidad
+    End Function
 End Class
