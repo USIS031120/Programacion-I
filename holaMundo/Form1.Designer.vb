b7a999a60d7896fabf7fc0cc277239a5e292ea61
Herson Geovanni Lopez Campos <usis031120@ugb.edu.sv>
2020-09-16 11:57:20 -0600

    Creacion de la rama sistemaInventarioFacturacion

--- holaMundo/Form1.Designer.vb
+++ holaMundo/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(585, 230)
+        Me.ClientSize = New System.Drawing.Size(577, 232)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.MaximizeBox = False
         Me.MinimizeBox = False
